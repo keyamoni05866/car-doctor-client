e9a29e9d6f9db3fb8016596543ab675a2f83a296
keyamoni05866 <keya05866@gmail.com>
2023-05-13 03:08:55 +0600

    first commit

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import banner1 from "../../../assets/images/banner/1.jpg";
+import banner2 from "../../../assets/images/banner/2.jpg";
+import banner3 from "../../../assets/images/banner/3.jpg";
+import banner4 from "../../../assets/images/banner/4.jpg";
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full h-[550px]  ">
+      <div id="slide1" className="carousel-item relative w-full ">
+        <img src={banner1} className="w-full rounded-xl" />
+        <div
+          className="absolute flex items-center  right-5 top-0 left-0 h-full
+    bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] rounded-xl"
+        >
+          <div className="text-white ml-11 space-y-7 w-1/3 ">
+            <h2 className="text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+
+            <div>
+              <button className="btn btn-error mr-5">Discover More</button>
+              <button className="btn btn-outline btn-warning">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide4" className="btn btn-circle mr-5">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={banner2} className="w-full rounded-xl" />
+        <div
+          className="absolute flex items-center  right-5 top-0 left-0 h-full
+    bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] rounded-xl"
+        >
+          <div className="text-white ml-11 space-y-7 w-1/3 ">
+            <h2 className="text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+
+            <div>
+              <button className="btn btn-error mr-5">Discover More</button>
+              <button className="btn btn-outline btn-warning">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide1" className="btn btn-circle mr-5">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={banner3} className="w-full rounded-xl" />
+        <div
+          className="absolute flex items-center  right-5 top-0 left-0 h-full
+    bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] rounded-xl"
+        >
+          <div className="text-white ml-11 space-y-7 w-1/3 ">
+            <h2 className="text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+
+            <div>
+              <button className="btn btn-error mr-5">Discover More</button>
+              <button className="btn btn-outline btn-warning">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide2" className="btn btn-circle mr-5">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={banner4} className="w-full rounded-xl" />
+        <div
+          className="absolute flex items-center  right-5 top-0 left-0 h-full
+    bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] rounded-xl"
+        >
+          <div className="text-white ml-11 space-y-7 w-1/3 ">
+            <h2 className="text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+
+            <div>
+              <button className="btn btn-error mr-5">Discover More</button>
+              <button className="btn btn-outline btn-warning">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide3" className="btn btn-circle mr-5">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import About from '../About/About';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <About></About>
+            <h2>this is home</h2>
+        </div>
+    );
+};
+
+export default Home;
